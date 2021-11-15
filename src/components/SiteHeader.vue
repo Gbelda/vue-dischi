3917ae0a15424d04c72aa5978f43fd911047578f
gbelda <gbelda56@gmail.com>
2021-11-15 12:25:25 +0100

    installed axios

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="container-fluid">
+    <div class="header">
+      <img src="../assets/img/spotify.png" alt="" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang='scss'>
+@import "../assets/scss/variables.scss";
+header {
+  background-color: $secondary_bg;
+  .header {
+    height: 80px;
+    line-height: 70px;
+    padding-left: 20px;
+    img {
+      height: 60px;
+    }
+  }
+}
+</style>
---
+++ src/components/SiteMain.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-6"></div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      albums: [],
+      error: "",
+    };
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response) => {
+        this.albums = response.data.response;
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+main {
+  min-height: calc(100vh - 80px);
+  max-height: calc(100vh - 80px);
+  background-color: $main_bg;
+}
+</style>
